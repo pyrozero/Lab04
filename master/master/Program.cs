ec83b28444b14c14ded582fad586cf23f9973576
Falish <pyro.zero@hotmail.com>
2017-10-29 02:52:24 +0700

    การทดลอง การใช้งานคำสั่ง foreach…in

--- master/master/Program.cs
+++ master/master/Program.cs
@@ -1,22 +1,44 @@
 ﻿using System;
-public class GotoTest
+
+namespace ConsoleApp2
 {
-    static void Main(string[] args)
+    class ForEachTest
     {
-        Console.WriteLine("Line 1");
-        goto line4;
-        Console.WriteLine("Line 2");
-        Console.WriteLine("Line 3");
-        line4:
-        Console.WriteLine("Line 4");
-        Console.WriteLine("Line 5");
-        goto line1;
-        Console.WriteLine("Line 6");
-        
-        Console.WriteLine("Line 7");
-        Console.WriteLine("Line 8");
-        Console.WriteLine("Line 9");
-        line1:
-        Console.WriteLine("Line 10");
+        static void Main(string[] args)
+        {
+            // Initial string array with month names.
+            string[] monthName = new string[] { "January","February",
+                         "March","April","May","June","July","August",
+                         "September","October","November","December"};
+
+            Console.WriteLine("-------- foreach,in loop");
+            foreach (string month in monthName)
+            {
+                System.Console.WriteLine(month);
+            }
+            System.Console.WriteLine();
+
+            // Compare the previous loop to a similar for loop.
+            Console.WriteLine("-------- for loop");
+            for (int i = 0; i < monthName.Length; i++)
+            {
+                System.Console.WriteLine(monthName[i]);
+            }
+            System.Console.WriteLine();
+
+
+            // You can maintain a count of the elements in the collection.
+            int count = 0;
+            foreach (string month in monthName)
+            {
+                count += 1;
+                System.Console.WriteLine("Element #{0}: {1}", count, month);
+            }
+            System.Console.WriteLine("Number of elements in the array: {0}", count);
+
+            // Keep the console open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
     }
 }
