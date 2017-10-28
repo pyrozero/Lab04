75a5361a1ceb7931f5f19dfc7c3891a0859e1699
Falish <pyro.zero@hotmail.com>
2017-10-28 23:50:11 +0700

    การเปลี่ยนทิศทางแบบไม่มีเงื่อนไข

--- master/master/Program.cs
+++ master/master/Program.cs
@@ -1,15 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace master
+public class GotoTest
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-        }
+        Console.WriteLine("Line 1");
+        goto line4;
+        Console.WriteLine("Line 2");
+        Console.WriteLine("Line 3");
+        line4:
+        Console.WriteLine("Line 4");
+        Console.WriteLine("Line 5");
+        goto line1;
+        Console.WriteLine("Line 6");
+        
+        Console.WriteLine("Line 7");
+        Console.WriteLine("Line 8");
+        Console.WriteLine("Line 9");
+        line1:
+        Console.WriteLine("Line 10");
     }
 }
